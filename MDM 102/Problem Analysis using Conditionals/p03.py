d4621d0c35e626c688a4dcdf9cde2b2ba30f762d
Shishir Halder <s.halder0108@gmail.com>
2022-11-06 07:45:58 +0600

    solution of problem 2

---
+++ MDM 102/Problem Analysis using Conditionals/p03.py
@@ -0,0 +1,12 @@
+# A python program to check 
+# whether a number is 
+# positive or negative or equal to zero.
+
+num = float(input("Enter number: "))
+
+if num == 0:
+    print("Zero.")
+elif num > 0:
+    print("Positive.")
+else:
+    print("Negative.")
